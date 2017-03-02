c710b2333cd748d609aeca50689bc2794104eed9
Felipe Martins <me@fefas.net>
2017-03-02 09:02:26 -0300

    Add missing method in StatusCodeInterface

--- src/StatusCodeInterface.php
+++ src/StatusCodeInterface.php
@@ -4,6 +4,7 @@ namespace Bauhaus\Http\Response;
 
 interface StatusCodeInterface
 {
+    public function code(): int;
     public function class(): string;
     public function reasonPhrase(): ?string;
 }
